08bd908a47ddc07d672c02329602146726b446d4
Pawel Bochnacki <you@example.com>
2023-04-23 13:43:07 +0200

    Initial commit

---
+++ src/main/java/com/costumeshop/CostumeshopApplication.java
@@ -0,0 +1,13 @@
+package com.costumeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CostumeshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CostumeshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/costumeshop/CostumeshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.costumeshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CostumeshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
